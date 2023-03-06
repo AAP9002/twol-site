441dfca8abb75ebcc68608514eedba5defea5d2b
aap9002 <aap9002@gmail.com>
2023-03-06 11:30:48 +0000

    game state

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,14 +3,11 @@ import background from '../../images/General/Background.jpg'
 import smallLogo from '../../images/General/TWOL Logo 2021.jpg'
 const Home = () => {
     return(<>
+      <b className='Opening_Line'>rising from the ashes of the 1980’s Mod culture comes a modern-day story of five undying friendships stained by gangland violence, loss and bloody revenge...</b>
       <div className='backgroundImageWrapper'>
         <img src={background} className={'backgroundImage'} alt="Scooter on floor"/>
       </div>
       <img src={smallLogo} className={'HomeLogo'} alt="Home page logo"/>
-
-      <div>
-        <b className='Opening_Line'>rising from the ashes of the 1980’s Mod culture comes a modern-day story of five undying friendships stained by gangland violence, loss and bloody revenge...</b>
-      </div>
 
       <div>
         <h1>UPDATES</h1>
