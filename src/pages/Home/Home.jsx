0251568298bb553d33c57ff8568d4dcec2c73cb7
z72819ap <alan.prophett@student.manchester.ac.uk>
2024-05-19 15:42:38 +0100

    improve logo viability and change vid background

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -12,7 +12,10 @@ const Home = () => {
     <div className='backgroundImageWrapper'>
       <img src={background} className={'backgroundImage'} alt="Scooter on floor" />
     </div>
-    <img src={smallLogo} className={'HomeLogo'} alt="Home page logo" />
+    <div className='d-flex'>
+      <img src={smallLogo} className={'LogoBack'} alt="Home page logo" />
+      <img src={smallLogo} className={'HomeLogo'} alt="Home page logo" />
+    </div>
     <b className='Opening_Line'>Rising from the ashes of the 1980`s Mod culture comes a modern-day story of five undying friendships stained by gangland violence, loss and bloody revenge...</b>
     <br />
     <br />
